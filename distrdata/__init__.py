1036c30b6e969e5d5d5f822e3dabfb2251fc49d9
Ilya <ilya.vouk@gmail.com>
2019-10-26 15:14:45 +0300

    Add normal distribution generator

--- distrdata/__init__.py
+++ distrdata/__init__.py
@@ -1,1 +1,13 @@
-from ._generator_base import *
+from ._generator_base import generate_dataset
+
+
+import scipy.stats as stats
+
+
+def generate_dataset_normal(range_of_values, *args, **kwargs):
+    return generate_dataset(
+        range_of_values,
+        stats.norm,
+        *args,
+        **kwargs
+    )
--- test_main.py
+++ test_main.py
@@ -8,12 +8,14 @@ class TestGeneratorBase:
             100,
             norm
         )
+        assert data.shape == (100, 2)
 
     def test_radius_tuple(self):
         data = distrdata.generate_dataset(
             (-10, 10, 0.1),
             norm
         )
+        assert data.shape == (200, 2)
 
     def test_upper_border(self):
         data = distrdata.generate_dataset(
@@ -22,3 +24,9 @@ class TestGeneratorBase:
             upper_border=10.0
         )
         assert data[:, 1].max() == 10.0
+
+
+class TestGeneratorNorm:
+    def test_generation(self):
+        data = distrdata.generate_dataset_normal(100)
+        assert data.shape == (100, 2)
